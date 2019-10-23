6f727c6efe8c0a8781a1d0e6fedef7ba0184c34c
xebraj <neff.esea@gmail.com>
2019-10-22 20:42:44 -0400

    Read available hours from API

--- app/src/main/java/com/vetmypet/vetmypet/model/Hourinterval.kt
+++ app/src/main/java/com/vetmypet/vetmypet/model/Hourinterval.kt
@@ -1,6 +1,6 @@
 package com.vetmypet.vetmypet.model
 
-data class Hourinterval(val start: String, val end: String) {
+data class HourInterval(val start: String, val end: String) {
     override fun toString(): String {
         return "$start - $end"
     }
